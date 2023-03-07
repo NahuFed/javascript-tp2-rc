aceba7eb61c67d687ac7ad4549da8ccb97ca4fcf
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-07 18:25:33 -0300

    validado ejercicio 4

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -2,18 +2,27 @@
 // número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con
 // “cancelar” deberá indicarse la suma total de los números introducidos.
 
-let numero = parseInt(prompt("ingrese un numero"));
+let numero = 0;
 let aux = 0;
 
-while (confirm("presione cancelar para salir o aceptar para sumar un numero")) {
-  do {
-    aux = parseInt(prompt("ingrese un numero"));
-    if (isNaN(aux) === false) {
-      numero += aux;
-    } else {
-      alert("no es un numero");
+do {
+  numero = parseInt(prompt("ingrese un numero"));
+  if (isNaN(numero) === false) {
+    while (
+      confirm("presione cancelar para salir o aceptar para sumar un numero")
+    ) {
+      do {
+        aux = parseInt(prompt("ingrese un numero"));
+        if (isNaN(aux) === false) {
+          numero += aux;
+        } else {
+          alert("no es un numero");
+        }
+      } while (isNaN(aux) === true);
     }
-  } while (isNaN(aux) === true);
-}
+  } else {
+    alert("no es un numero");
+  }
+} while (isNaN(numero) === true);
 
 document.write("Resultado de la suma: <br>" + numero);
