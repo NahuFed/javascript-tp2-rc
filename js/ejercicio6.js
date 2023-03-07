93ffc9e81bb46f526114d911991e0184dcd09767
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-07 19:02:22 -0300

    agregado ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,23 @@
+// 6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+
+
+let numero = 0
+do{
+    numero=parseInt(prompt('ingrese un numero del 1 al 30'))
+if (numero>=1 && numero<=30){
+    for(i=1 ; i <= numero;i++){
+        for(j=1 ; j<= i; j++){
+            document.write(i)
+        }
+        document.write('<br>')
+    }
+}else{
+    alert('no es un numero valido')
+}
+}while(isNaN(numero)===true) 
