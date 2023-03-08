5e0760242aca1a9e46f6fe2b59bb5987d16db33c
Nahuel Rodriguez <nahufed@gmail.com>
2023-03-07 23:18:51 -0300

    agregado ejercicio 14

---
+++ js/ejercicio14.js
@@ -0,0 +1,13 @@
+// 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo
+// – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué
+// tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
+
+let texto = prompt('ingrese texto')
+
+for(i=0;i<texto.length;i++){
+    if(i<(texto.length - 1))
+    document.write(texto.charAt(i)+'-')
+    else{
+        document.write(texto.charAt(i))
+    }
+}
